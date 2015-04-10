2c2750d229e74f2b1d1ad37820e3fe7610e53e31
kaneplusplus <kaneplusplus@gmail.com>
2015-04-10 17:41:18 -0400

    Working on an example that gets the cointegration for the sp500.

--- scripts/cointegration.r
+++ scripts/cointegration.r
@@ -1,24 +1,36 @@
 library(devtools)
+library(doMC)
+library(iotools)
+library(xts)
+registerDoMC(cores=16)
 
 document("..")
+col_types=c(rep("character",3), rep("numeric", 3), rep("character", 2))
 
-x = iotools:::read.table.raw(
-  "/Users/mike/projects/jackson_lecture/may_trades/taq_20100506_trades_all.csv",
-  header=TRUE, sep=",")
+x = dstrsplit(readAsRaw("/home/mkane/taq_20100506_trades_all.csv"),
+              sep=",", skip=1, col_types=col_types)              
+names(x) = c("symbol", "date", "time", "price", "size", "corr", "cond", "ex")	
 
-names(x) = tolower(names(x))
 # Remove bunk trades.
 x = na.omit(x[!(x$cond %in% c("L", "N", "O", "Z", "P")),1:5])
 x = x[x$size > 0,]
 
 sym_split = split(1:nrow(x), x$symbol)
 x$time_stamp = paste(x$date, x$time)
-x$date_time = strptime(x$time_stamp, format="%Y%m%d %H:%M:%S", tz=Sys.timezone())
+x$date_time=strptime(x$time_stamp, format="%Y%m%d %H:%M:%S", tz=Sys.timezone())
 
 data(sp)
 
 sp_split = sym_split[names(sym_split) %in% sp$symbol]
-sp_stocks = foreach(inds=sp_split, .combine=cbind) %dopar% {
-  vwap(x$price[inds], x$size[inds], x$date_time[inds])
+sp_stocks = foreach(inds=sp_split, .combine=cbind) %do% {
+  xts(cbind(x$price[inds], x$size[inds]), order.by=x$date_time[inds])
 }
-names(sp_stocks) = names(sp_split)
+psp = matrix(1:ncol(sp_stocks), ncol=2, byrow=TRUE)
+sp_stocks2 = foreach(i = 1:nrow(psp), .combine=cbind) %dopar% {
+  vwap(as.vector(sp_stocks[,psp[i, 1]]), as.vector(sp_stocks[,psp[i,2]]),
+       time(sp_stocks))
+}
+
+sp_stocks2 = foreach(j=1:ncol(sp_stocks), .combine=cbind) %dopar% {
+  carry_price_forward(sp_stocks[,j])
+}
