07c47dbe13ee2df77928ac9fcaa6a2bcb4ea4284
kaneplusplus <kaneplusplus@gmail.com>
2015-04-12 15:49:03 -0400

    More cointegration additions and related utilities.

--- scripts/cointegration.r
+++ scripts/cointegration.r
@@ -2,6 +2,8 @@ library(devtools)
 library(doMC)
 library(iotools)
 library(xts)
+library(devtools)
+library(doMC)
 registerDoMC(cores=16)
 
 document("..")
@@ -20,17 +22,27 @@ x$time_stamp = paste(x$date, x$time)
 x$date_time=strptime(x$time_stamp, format="%Y%m%d %H:%M:%S", tz=Sys.timezone())
 
 data(sp)
+x = x[x$symbol %in% sp$symbol,]
 
-sp_split = sym_split[names(sym_split) %in% sp$symbol]
-sp_stocks = foreach(inds=sp_split, .combine=cbind) %do% {
-  xts(cbind(x$price[inds], x$size[inds]), order.by=x$date_time[inds])
-}
-psp = matrix(1:ncol(sp_stocks), ncol=2, byrow=TRUE)
-sp_stocks2 = foreach(i = 1:nrow(psp), .combine=cbind) %dopar% {
-  vwap(as.vector(sp_stocks[,psp[i, 1]]), as.vector(sp_stocks[,psp[i,2]]),
-       time(sp_stocks))
+# x is a data frame with a price, size, and symbol column.
+clean_and_normalize_transactions = function(x, on="minutes", k=1) {
+  sp_split = split(1:nrow(x), x$symbol)
+  x = foreach(inds=sp_split, .combine=cbind) %do% {
+    xts(cbind(x$price[inds], x$size[inds]), order.by=x$date_time[inds])
+  }
+  psp = matrix(1:ncol(x), ncol=2, byrow=TRUE)
+  # The following is a hog and it needs to be better. Should vwap return an
+  # xts object or would it be better as a data frame?
+  x = foreach(i = 1:nrow(psp), .combine=cbind) %dopar% {
+    vwap(as.vector(x[,psp[i, 1]]), as.vector(x[,psp[i,2]]),
+         time(x), on=on, k=k)
+  }
+  x = foreach(j=1:ncol(x), .combine=cbind) %dopar% {
+    carry_price_forward(x[,j])
+  }
+  colnames(x) = names(sp_split)
+  x
 }
 
-sp_stocks2 = foreach(j=1:ncol(sp_stocks), .combine=cbind) %dopar% {
-  carry_price_forward(sp_stocks[,j])
-}
+sp_trades = clean_and_normalize_transactions(x, on="seconds", k=1)
+
