c9666bd8d3f4119f8e6e87ea6b5f95a96bf4e00d
kaneplusplus <kaneplusplus@gmail.com>
2015-05-20 10:42:10 -0400

    Code hardening.

--- R/cointegration.r
+++ R/cointegration.r
@@ -3,7 +3,8 @@
 #'
 #' @param x a data.frame, matrix, or xts object
 #' @return An upper triangular matrix where each entry corresponds to the 
-#' p-value from McKinnons's test assuming no time trend.
+#' p-value from McKinnons's test assuming no time trend. Note that if one
+#' stock in a pair-wise cointegration is constant the return value will be 1.
 #' @examples
 #' data(aapl_fix)
 #' data(a_fix)
@@ -28,25 +29,51 @@ cointegration_p_matrix = function(x) {
                  dimnames=list(names(x), names(x)))
       for (j in 1:ncol(it)) {
         # Fit the cointegration.
-        y_vec = as.matrix(x)[,it[1,j],drop=FALSE]
-        x_vec = cbind(as.matrix(x)[,it[2,j],drop=FALSE], rep(1, nrow(y_vec)))
-        qr_x = Matrix::qr(x_vec)
-        if (qr_x$rank < 2) {
-          x_vec = x_vec[, 1, drop=FALSE]
-          qr_x = Matrix::qr(x_vec)
+        val = try({
+          p_val = 1
+          y_vec = as.matrix(x)[,it[1,j],drop=FALSE]
+          x_vec = cbind(as.matrix(x)[,it[2,j],drop=FALSE], rep(1, nrow(y_vec)))
+
+          # Get the summed differences for y and x. If either is a vector
+          # of constant value, handle it.
+          x_sum_diff = sum(diff(x_vec[,1]))
+          y_sum_diff = sum(diff(y_vec))
+          if (x_sum_diff != 0 && y_sum_diff != 0) {
+            # Neither vector is constant. Test for cointegration.
+            qr_x = Matrix::qr(x_vec)
+            if (qr_x$rank < 2) {
+              x_vec = x_vec[, 1, drop=FALSE]
+              qr_x = Matrix::qr(x_vec)
+            }
+            qr_solve = qr.solve(x_vec, y_vec)
+            qr_coef = qr.coef(qr_x, y_vec)
+            resids = na.omit(y_vec - x_vec %*% qr_coef)
+            p_val = as.vector(unitrootTest(resids, type="nc")@test$p.value[1])
+          } else {
+            # At least one vector is constant. Print a warning.
+            if (y_sum_diff == 0 && x_sum_diff == 0) {
+              warning(paste(it[1,j], "and", it[2,j], "are constant."))
+            } else if (y_sum_diff == 0) {
+              warning(paste(it[1,j], "is constant."))
+            } else {
+              warning(paste(it[2,j], "is constant."))
+            }
+          }
+          p_val
+        })
+        # Check for other problems. 
+        if (inherits(val, "try-error")) {
+          warning(paste("Problem at", it[1,j], it[2,j]))
+          val = NA
         }
-        qr_solve = qr.solve(x_vec, y_vec)
-        qr_coef = qr.coef(qr_x, y_vec)
-        resids = na.omit(y_vec - x_vec %*% qr_coef)
-        val=as.vector(unitrootTest(resids, type="nc")@test$p.value[1])
         m[it[1,j], it[2,j]] = val
       }
       m
-    }    
+    }
     r[lower.tri(r)]=0
   }
   r
-}     
+}
 
 #' Get cointegration information for time series data
 #'
